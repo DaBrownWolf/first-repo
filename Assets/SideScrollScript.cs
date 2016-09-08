bef9ff73cf4767d0c8e5cc5524ca8c6093996d42
Atirath-PC\Atirath <atikosireddy98@gmail.com>
2016-09-08 12:12:07 -0700

    no message

---
+++ Assets/SideScrollScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideScrollScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+            MeshRenderer mr = GetComponent<MeshRenderer>();
+
+            Material mat = mr.material;
+
+            Vector2 offset = mat.mainTextureOffset;
+
+            offset.x += Time.deltaTime;
+
+            mat.mainTextureOffset = offset;
+
+            gameObject.GetComponent<Animation>()["animationName"].wrapMode = WrapMode.PingPong;
+
+    }
+}
